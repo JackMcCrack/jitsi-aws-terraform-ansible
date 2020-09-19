b6a9b7f36afdeadb7c9d7c9a9ba79f7dc550077f
JackMcCrack <jack@charon.lan>
2020-09-20 00:55:00 +0900

    WIP: jitsi packages are installed but not configured :(

--- main.tf
+++ main.tf
@@ -2,11 +2,15 @@
 #	Setup a AWS EC2 Instance via Terraform and install Jitis-Meet via Ansible
 ####
 
-# version 20200919-1
+# version 20200919-2
 
 variable "aws_key_name" {
   type = string
   default = "test-deleteme" # Name of your SSH Key pair
+}
+variable "aws_key_file" {
+  type = string
+  default = "~/.ssh/id_rsa.aws" # Name of your SSH Key pair
 }
 variable "aws_region" {
   type = string
@@ -75,9 +79,10 @@ resource "aws_route_table" "route" {
 # Subnet for Jits instance(s)
 resource "aws_subnet" "jitsi-meet-net" {
   vpc_id = aws_vpc.ip_range.id
+  availability_zone = "${var.aws_region}${var.aws_availability_zone}"
   cidr_block = "10.171.234.16/28"
   assign_ipv6_address_on_creation = true
-  ipv6_cidr_block = aws_vpc.ip_range.ipv6_cidr_block
+  ipv6_cidr_block = cidrsubnet(aws_vpc.ip_range.ipv6_cidr_block, 8, 1)
   tags = {
     name = "Jitsi_Meet"
   }
@@ -95,6 +100,7 @@ resource "aws_route_table_association" "jitsi-meet-net_route" {
 #INCOMING
 resource "aws_security_group" "ssh" {
   name = "allow ssh"
+  vpc_id =      aws_vpc.ip_range.id
   ingress {
     description = "ssh"
     from_port   = 22
@@ -109,6 +115,7 @@ resource "aws_security_group" "ssh" {
 }
 resource "aws_security_group" "webserver" {
   name = "allow http and https"
+  vpc_id =      aws_vpc.ip_range.id
   ingress {
     description = "http"
     from_port   = 80
@@ -156,6 +163,7 @@ resource "aws_security_group" "jitsi" {
 }
 resource "aws_security_group" "ping" {
   name =        "allow icmp and icmpv6 echo requests"
+  vpc_id =      aws_vpc.ip_range.id
   ingress {
     description = "icmp echo request (ping)"
     protocol    = "icmp"
@@ -178,6 +186,7 @@ resource "aws_security_group" "ping" {
 #FIXME: allow only required ports / by service
 resource "aws_security_group" "output" {
   name =        "allow all output"
+  vpc_id =      aws_vpc.ip_range.id
   egress {
     from_port   = 0
     to_port     = 0
@@ -195,9 +204,6 @@ resource "aws_security_group" "output" {
 
 resource "aws_network_interface" "jitsi-nic" {
   subnet_id = aws_subnet.jitsi-meet-net.id
-  private_ips = [
-  "10.171.234.20"
-  ]
   security_groups = [ 
     aws_security_group.ssh.id, 
     aws_security_group.webserver.id, 
@@ -207,8 +213,16 @@ resource "aws_network_interface" "jitsi-nic" {
   ]
 }
 
+resource "aws_eip" "jitsi_public_ip" {
+  vpc               = true
+  network_interface = aws_network_interface.jitsi-nic.id
+  depends_on = [
+    aws_internet_gateway.default_gw
+  ]
+}
+
 resource "aws_instance" "jitsi-instance" {
-  ipv6_address_count = 1
+  #ipv6_address_count = 1
   ami               = "ami-06b258aac2ae736c2" 	# ubuntu 20.04 ARM64
   instance_type     = "t4g.micro"		# ARM (free trail: https://aws.amazon.com/de/blogs/aws/new-t4g-instances-burstable-performance-powered-by-aws-graviton2/)
   availability_zone = "${var.aws_region}${var.aws_availability_zone}"
@@ -225,9 +239,15 @@ resource "aws_instance" "jitsi-instance" {
   }
 }
 
+
+
 output "server_public_ip4" {
   value = aws_instance.jitsi-instance.public_ip
 }
 output "server_public_ip6" {
   value = aws_instance.jitsi-instance.ipv6_addresses
+}
+
+output "run_ansible" {
+  value = "echo -e '[jitsi]\n${aws_instance.jitsi-instance.public_ip}\n\n[jitsi:vars]\nansible_ssh_private_key_file=${var.aws_key_file}' >ansible/hosts;  sleep 120; ansible-playbook -i ansible/hosts ansible/jitsi-playbook.yml"
 }
